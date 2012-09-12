0df3c657bce1773b5cc729b921b5b3db24eb2409
Tristan Lins <tristan.lins@infinitysoft.de>
2012-09-12 11:52:15 +0200

    Add contao 3 autoload files.

---
+++ src/system/modules/metapalettes/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2012 Leo Feyer
+ * 
+ * @package Metapalettes
+ * @link    http://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'MetaPalettes' => 'system/modules/metapalettes/MetaPalettes.php',
+));
