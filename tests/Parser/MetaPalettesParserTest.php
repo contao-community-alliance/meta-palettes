361e073a066b8c29eb9938d4dbc57520140a0efa
David Molineus <david.molineus@netzmacht.de>
2017-09-18 15:01:16 +0200

    Fix file headers for test files.

--- tests/Parser/MetaPalettesParserTest.php
+++ tests/Parser/MetaPalettesParserTest.php
@@ -1,11 +1,14 @@
 <?php
 
 /**
- * @package    meta-palettes
- * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2017 netzmacht David Molineus. All rights reserved.
- * @filesource
+ * MetaPalettes for the Contao Open Source CMS
  *
+ * @package   MetaPalettes
+ * @author    David Molineus <david.molineus@netzmacht.de>
+ * @copyright 2013-2014 bit3 UG
+ * @copyright 2015-2017 Contao Community Alliance.
+ * @license   LGPL-3.0+ https://github.com/contao-community-alliance/meta-palettes/license
+ * @link      https://github.com/bit3/contao-meta-palettes
  */
 
 namespace ContaoCommunityAlliance\MetaPalettes\Test\Parser;
